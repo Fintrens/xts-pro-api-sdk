2c3323038cbb7b09cdbc6c31aa7e14309e8e36b9
AlbyAlex <alby.alex@fintrens.com>
2024-06-05 12:17:36 +0530

    User retry

--- xts-rest-api/xts-sdk/src/main/java/com/sf/xts/api/sdk/main/api/FintrensInteractiveClient.java
+++ xts-rest-api/xts-sdk/src/main/java/com/sf/xts/api/sdk/main/api/FintrensInteractiveClient.java
@@ -17,107 +17,102 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+
 /**
  * It provides all Interactive API methods
  *
  * @author SymphonyFintech
  */
-public class FintrensInteractiveClient extends FintrensConfigurationProvider {
-  private HttpClient httpClient = HttpClientBuilder.create().build();
-  Gson gson = new Gson();
-  private SocketHandler sh = null;
-  public String uniqueKey = null;
-  public String authToken = null;
-  public String interactiveURL = null;
-  public String user = null;
-  public boolean isInvestorClient = true;
-  public String clientID = null;
-  public Logger logger = LoggerFactory.getLogger(FintrensInteractiveClient.class);
-  Object object = new Object();
-  FintrensRequestHandler requestHandler;
-  XTSAPIInteractiveEvents xtsapiInteractiveEvents;
+public  class FintrensInteractiveClient extends FintrensConfigurationProvider {
+	private HttpClient httpClient = HttpClientBuilder.create().build();
+	Gson gson = new Gson();
+	private  SocketHandler sh=null;
+	public  String uniqueKey = null;
+	public  String authToken = null;
+	public  String interactiveURL = null;
+	public  String user = null;
+	public  boolean isInvestorClient = true;
+	public  String clientID = null;
+	public  Logger logger = LoggerFactory.getLogger(FintrensInteractiveClient.class);
+	Object object = new Object();
+	FintrensRequestHandler requestHandler;
+	XTSAPIInteractiveEvents xtsapiInteractiveEvents;
 
-  public FintrensInteractiveClient(XTSAPIInteractiveEvents xtsapiInteractiveEvents)
-      throws IOException {
-    loadConfiguration();
-    this.xtsapiInteractiveEvents = xtsapiInteractiveEvents;
-    requestHandler = new FintrensRequestHandler();
-  }
+	public FintrensInteractiveClient(XTSAPIInteractiveEvents xtsapiInteractiveEvents) throws IOException{
+		loadConfiguration();
+		this.xtsapiInteractiveEvents = xtsapiInteractiveEvents;
+		requestHandler = new FintrensRequestHandler();
+	}
 
-  public void addListner(XTSAPIInteractiveEvents obj) {
-    sh.addListner(obj);
-  }
+	public void addListner(XTSAPIInteractiveEvents obj ) {
+		sh.addListner(obj);
+	}
 
-  public void HostLookUp() throws APIException {
-    HttpPost request = new HttpPost(commonURL + port + hostLookUp);
-    request.addHeader("content-type", "application/json");
-    JSONObject data = new JSONObject();
-    data.put("accesspassword", accesspassword);
-    data.put("version", version);
-    String response = this.requestHandler.processPostHttpHostRequest(request, data, "HOSTLOOKUP");
-    JSONObject jsonObject = new JSONObject(response);
-    uniqueKey = (String) ((JSONObject) jsonObject.get("result")).get("uniqueKey");
-    interactiveURL = (String) ((JSONObject) jsonObject.get("result")).get("connectionString");
-  }
+	public void HostLookUp() throws APIException {
+		HttpPost request = new HttpPost(commonURL+port + hostLookUp);
+		request.addHeader("content-type", "application/json");
+		JSONObject data = new JSONObject();
+		data.put("accesspassword", accesspassword);
+		data.put("version", version);
+		String response = this.requestHandler.processPostHttpHostRequest(request, data, "HOSTLOOKUP");
+		JSONObject jsonObject = new JSONObject(response);
+		uniqueKey = (String)((JSONObject)jsonObject.get("result")).get("uniqueKey");
+		interactiveURL = (String)((JSONObject)jsonObject.get("result")).get("connectionString");
+	}
 
-  public String Login(String secretKey, String appKey) throws APIException {
-    this.HostLookUp();
-    HttpPost request = new HttpPost(interactiveURL + loginINT);
-    request.addHeader("content-type", "application/json");
-    JSONObject data = new JSONObject();
-    data.put("secretKey", secretKey);
-    data.put("appKey", appKey);
-    data.put("uniqueKey", uniqueKey);
-    data.put("source", source);
-    String response = this.requestHandler.processPostHttpRequest(request, data, "LOGIN", authToken);
-    if (response == null) {
-      return null;
-    } else {
-      JSONObject jsonObject = new JSONObject(response);
-      authToken = (String) ((JSONObject) jsonObject.get("result")).get("token");
-      user = (String) ((JSONObject) jsonObject.get("result")).get("userID");
-      isInvestorClient = (Boolean) ((JSONObject) jsonObject.get("result")).get("isInvestorClient");
-      if (authToken != null) {
-        initializeListner(this.xtsapiInteractiveEvents);
-      }
-      return authToken;
-    }
-  }
+	public String Login(String secretKey, String appKey) throws APIException {
+		this.HostLookUp();
+		HttpPost request = new HttpPost(interactiveURL + loginINT);
+		request.addHeader("content-type", "application/json");
+		JSONObject data = new JSONObject();
+		data.put("secretKey", secretKey);
+		data.put("appKey", appKey);
+		data.put("uniqueKey", uniqueKey);
+		data.put("source", source);
+		String response = this.requestHandler.processPostHttpRequest(request, data, "LOGIN",authToken);
+		if (response==null){
+			return null;
+		}
+		else {
+			JSONObject jsonObject = new JSONObject(response);
+			authToken = (String) ((JSONObject) jsonObject.get("result")).get("token");
+			user = (String) ((JSONObject) jsonObject.get("result")).get("userID");
+			isInvestorClient = (Boolean) ((JSONObject) jsonObject.get("result")).get("isInvestorClient");
+			if (authToken != null) {
+				initializeListner(this.xtsapiInteractiveEvents);
+			}
+			return authToken;
+		}
+	}
 
-  public Position getPosition(String posType) throws APIException {
-    String data =
-        requestHandler.processGettHttpRequest(
-            new HttpGet(interactiveURL + positions + "?dayOrNet=" + posType),
-            "POSITION",
-            authToken);
-    Position position = gson.fromJson(data, Position.class);
-    return position;
-  }
+	public Position getPosition(String posType)throws APIException {
+		String data = requestHandler.processGettHttpRequest(new HttpGet(interactiveURL + positions + "?dayOrNet=" + posType),"POSITION",authToken);
+		Position position = gson.fromJson(data, Position.class);
+		return position;
+	}
 
-  public PlaceOrderResponse PlaceOrder(PlaceOrderRequest placeOrderRequest) {
-    JSONObject placeOrderJson = new JSONObject();
-    placeOrderJson.put("exchangeSegment", placeOrderRequest.exchangeSegment);
-    placeOrderJson.put("exchangeInstrumentID", placeOrderRequest.exchangeInstrumentId);
-    placeOrderJson.put("productType", placeOrderRequest.productType);
-    placeOrderJson.put("orderType", placeOrderRequest.orderType);
-    placeOrderJson.put("orderSide", placeOrderRequest.orderSide);
-    placeOrderJson.put("timeInForce", placeOrderRequest.timeInForce);
-    placeOrderJson.put("disclosedQuantity", placeOrderRequest.disclosedQuantity);
-    placeOrderJson.put("orderQuantity", placeOrderRequest.orderQuantity);
-    placeOrderJson.put("limitPrice", placeOrderRequest.limitPrice);
-    placeOrderJson.put("stopPrice", placeOrderRequest.stopPrice);
-    placeOrderJson.put("orderUniqueIdentifier", placeOrderRequest.orderUniqueIdentifier);
-    String data =
-        requestHandler.processPostHttpRequest(
-            new HttpPost(interactiveURL + orderBook), placeOrderJson, "PLACEORDER", authToken);
-    PlaceOrderResponse placeOrderResponse = gson.fromJson(data, PlaceOrderResponse.class);
-    return placeOrderResponse;
-  }
+	public PlaceOrderResponse PlaceOrder(PlaceOrderRequest placeOrderRequest) {
+		JSONObject placeOrderJson = new JSONObject();
+		placeOrderJson.put("exchangeSegment", placeOrderRequest.exchangeSegment);
+		placeOrderJson.put("exchangeInstrumentID", placeOrderRequest.exchangeInstrumentId);
+		placeOrderJson.put("productType",placeOrderRequest.productType);
+		placeOrderJson.put("orderType", placeOrderRequest.orderType);
+		placeOrderJson.put("orderSide", placeOrderRequest.orderSide);
+		placeOrderJson.put("timeInForce", placeOrderRequest.timeInForce);
+		placeOrderJson.put("disclosedQuantity", placeOrderRequest.disclosedQuantity);
+		placeOrderJson.put("orderQuantity", placeOrderRequest.orderQuantity);
+		placeOrderJson.put("limitPrice", placeOrderRequest.limitPrice);
+		placeOrderJson.put("stopPrice", placeOrderRequest.stopPrice);
+		placeOrderJson.put("orderUniqueIdentifier", placeOrderRequest.orderUniqueIdentifier);
+		String data = requestHandler.processPostHttpRequest(new HttpPost(interactiveURL + orderBook),placeOrderJson,"PLACEORDER",authToken);
+		PlaceOrderResponse placeOrderResponse = gson.fromJson(data, PlaceOrderResponse.class);
+		return placeOrderResponse;
+	}
 
-  public boolean initializeListner(XTSAPIInteractiveEvents xtsapiInteractiveEvents) {
-    // Socket creating  for all the responses
-    sh = new SocketHandler(commonURL, user, authToken);
-    sh.addListner(xtsapiInteractiveEvents);
-    return true;
-  }
+	public  boolean initializeListner(XTSAPIInteractiveEvents xtsapiInteractiveEvents) {
+		//Socket creating  for all the responses
+		sh = new SocketHandler(commonURL, user, authToken);
+		sh.addListner(xtsapiInteractiveEvents);
+		return true;
+	}
 }
